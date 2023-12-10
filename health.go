efddedffa886a19968c7583e5ad678348d81363b
yamasaki <yamasakipann0218@gmail.com>
2023-12-11 03:05:13 +0900

    feat() : Add k8s cron & app init

--- health.go
+++ health.go
@@ -1,1 +1,30 @@
 package healthchecker
+
+import "os"
+
+func main() {
+	urls := getEnv()
+	for _, url := range urls {
+		go checkHealth(url)
+	}
+}
+
+func checkHealth(url string) {
+
+}
+
+func getEnv() []string {
+	var res []string
+	url := os.Getenv("URL1")
+	res = append(res, url)
+	url = os.Getenv("URL2")
+	res = append(res, url)
+	url = os.Getenv("URL3")
+	res = append(res, url)
+	url = os.Getenv("URL4")
+	res = append(res, url)
+	url = os.Getenv("URL5")
+	res = append(res, url)
+
+	return res
+}
